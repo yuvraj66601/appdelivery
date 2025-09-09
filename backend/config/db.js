ba970448355c5677c0a621bf3ae8424206e884fe
yuvraj66601 <ksdevyuvraj1@gmail.com>
2025-09-10 01:57:20 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('mongodb+srv://devyuvraj487:yuviabhiisbhai@cluster0.fiyy1yd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log("DB Connected"));
+    await mongoose.connect('yoyo').then(()=>console.log("DB Connected"));
    
 }
 
